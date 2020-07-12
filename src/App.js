d9f253b07adddd1b16e124a846d79a56a1535b74
Kerry Smith <kerrysfs@gmail.com>
2020-07-11 23:40:10 -0700

    added slider and hooked it up to the color brightness/darkness

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import Palette from './Palette';
-import seedColors from './seedColors';
-import { generatePalette }from './colorHelpers';
+import React, { Component } from "react";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
   render() {
-    console.log(generatePalette(seedColors[4]))
     return (
-      <div>
-        <Palette {...seedColors[4]} />
-      </div>
-    )
+       <div>
+         <Palette palette={generatePalette(seedColors[4])} />
+       </div>
+    );
   }
 }
 
