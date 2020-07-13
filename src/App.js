49dc081ce09adb347109e4b99ab070d9ac991aaf
Kerry Smith <kerrysfs@gmail.com>
2020-07-13 15:51:46 -0700

    added new route and link to color box

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
             />
           )}
         />
+        <Route path='/palette/:paletteId/:colorId' render={() => <h1>single page</h1>}/>
       </Switch>
 
       // <div>
