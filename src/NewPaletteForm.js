2d3220a4a4c4363713af8ea14eda36934acd75ff
Kerry Smith <kerrysfs@gmail.com>
2020-07-21 12:59:04 -0700

    styled colorpicker form, made palette larger, centered, etc

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -27,7 +27,9 @@ const styles = theme => ({
     flexShrink: 0
   },
   drawerPaper: {
-    width: drawerWidth
+    width: drawerWidth,
+    display: 'flex',
+    alignItems: 'center'
   },
   drawerHeader: {
     display: "flex",
@@ -52,6 +54,20 @@ const styles = theme => ({
       duration: theme.transitions.duration.enteringScreen
     }),
     marginLeft: 0
+  },
+  container: {
+    width: '90%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%'
+  },
+  buttons: {
+    width: '100%'
+  },
+  button: {
+    width: '50%'
   }
 });
 
@@ -151,12 +167,14 @@ class NewPaletteForm extends Component {
              </IconButton>
            </div>
            <Divider />
-           <Typography variant='h4'>Design Your Palette</Typography>
-           <div>
+           <div className={classes.container}>
+           <Typography variant='h4' gutterBottom>Design Your Palette</Typography>
+           <div className={classes.buttons}>
              <Button
                 variant='contained'
                 color='secondary'
                 onClick={this.clearColors}
+                className={classes.button}
              >
                Clear Palette
              </Button>
@@ -165,6 +183,7 @@ class NewPaletteForm extends Component {
                 color='primary'
                 onClick={this.addRandomColor}
                 disabled={paletteIsFull}
+                className={classes.button}
              >
                Random Color
              </Button>
@@ -174,6 +193,7 @@ class NewPaletteForm extends Component {
               addNewColor={this.addNewColor}
               colors={colors}
            />
+           </div>
          </Drawer>
          <main
             className={classNames(classes.content, {
