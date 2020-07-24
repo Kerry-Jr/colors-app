537d57800378831d9334a2a62706920821606817
Kerry Smith <kerrysfs@gmail.com>
2020-07-23 22:14:18 -0700

    make palette list some what responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes";
+
 export default {
   root: {
     backgroundColor: "blue",
@@ -11,7 +13,13 @@ export default {
     display: "flex",
     alignItems: "flex-start",
     flexDirection: "column",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%"
+    },
+    [sizes.down("xs")]: {
+      width: "70%"
+    }
   },
   nav: {
     display: "flex",
@@ -29,6 +37,23 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%"
+    gridGap: "5%",
+    // [sizes.down("lg")]: {
+    //   width: "25% !important",
+    //   height: "33.3333% !important"
+    // },
+    [sizes.down("md")]: {
+      // width: "50%",
+      gridTemplateColumns: "repeat(2, 50%)"
+    },
+    [sizes.down("sm")]: {
+      width: "80% !important",
+      height: "10% !important",
+      gridTemplateColumns: "repeat(1, 100%)"
+    },
+    [sizes.down("xs")]: {
+      width: "100%",
+      gridTemplateColumns: "repeat(1, 130%)"
+    }
   }
 };
